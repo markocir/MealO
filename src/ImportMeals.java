19577953b676f8c3effafe69a905fd342e523082
markocir <marc88cap@gmail.com>
2018-02-22 22:54:09 +0100

    On call execute if error in file structure is found (if cells contain null) an NullPointerException is thrown, the confirm dialog is shown to the user to inform him about the error and the import process is terminated: Nothing is INSERTed into the database table. Only when all rows from file are valid the database INSERT sequence is executed.

--- src/ImportMeals.java
+++ src/ImportMeals.java
@@ -2,8 +2,10 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -12,24 +14,27 @@ import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
+/**
+ *
+ * @author Marko Čirič <https://github.com/markocir>
+ */
 class ImportMeals extends ActionSettings {
     private Sheet sheet = null;
-    private Cell date = null;
-    private Cell shift = null;
-    private Cell mealNumber = null;
-    private Cell mealDescription = null;
-    private Cell allergens = null;
+    private String date = null;
+    private int shift = -1;
+    private int mealNumber = -1;
+    private String mealDescription = null;
+    private String allergens = null;
     private Database database = null;
     private MealSettings container = null;
-    private CustomTableModel tableModel;
+    private CustomTableModel tableModel = null;
     
-    public ImportMeals(MealSettings container, Database database, CustomTableModel tableModel)
+    public ImportMeals(MealSettings container, Database database, CustomTableModel tableModel) throws FileNotFoundException, IOException
     {
         super(database);
         this.database = database;
@@ -39,18 +44,10 @@ class ImportMeals extends ActionSettings {
         InputStream input = null;
         try 
         {
-            input = new FileInputStream("src/import/Mealo_import_scheme.xlsx");
+            input = new FileInputStream("src/import/Mealo_import_schem.xlsx");
             
             Workbook wb = WorkbookFactory.create(input);
             sheet = wb.getSheetAt(0);
-        } 
-        catch (FileNotFoundException ex) 
-        {
-            Logger.getLogger(ImportMeals.class.getName()).log(Level.SEVERE, null, ex);
-        } 
-        catch (IOException ex)
-        {
-            Logger.getLogger(ImportMeals.class.getName()).log(Level.SEVERE, null, ex);
         } 
         catch (InvalidFormatException ex) 
         {
@@ -66,65 +63,82 @@ class ImportMeals extends ActionSettings {
     public void execute() {
         String[] options = {"Yes", "No"};
         int option = JOptionPane.showOptionDialog(container, "Are you sure you want to import file data?", "Double-check import", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,null,options,options[1]);
+        
         if(option == JOptionPane.YES_OPTION)
             try 
             {
+                // start reading file on row
                 int row = 4;
-
+                
+                ArrayList<Meal> mealList = new ArrayList<>(25);
+                
                 while(readRow(row++))
                 {
-                    
-                    String query = String.format(""
-                            + "INSERT INTO mo_meals (mealNumber, shift, date, description, allergens) "
-                            + "VALUES (%d, %d, '%s', '%s', '%s')",
-                            (int)mealNumber.getNumericCellValue(), 
-                            (int)shift.getNumericCellValue(), 
-                            formatDate(String.valueOf(date.getDateCellValue())), 
-                            mealDescription.getStringCellValue(), 
-                            (allergens != null) ? allergens.getStringCellValue() : "");
-                    
-                    int key = database.executeQuery(query, true);
-                    
-                    if(container.getShiftNumber() == (int)shift.getNumericCellValue())
-                        tableModel.addRow(new Object[] {
-                            key, 
-                            formatDate(String.valueOf(date.getDateCellValue())), 
-                            (int)mealNumber.getNumericCellValue(),
-                            mealDescription.getStringCellValue(),
-                            (allergens != null) ? allergens.getStringCellValue() : ""
-                        });
+                    mealList.add(
+                            new Meal(
+                                    -1, 
+                                    mealNumber,
+                                    shift,
+                                    String.valueOf(date),
+                                    mealDescription,
+                                    allergens)
+                    );
                 }
                 
-                JOptionPane.showConfirmDialog(container, "Data has been imported.", "Import Successful", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE);
+                try 
+                {
+                    mealList = database.importMeals(mealList);
+                    
+                    for(Meal meal : mealList)
+                    {
+                        // add to table only meals matching current shift
+                        if(container.getShiftNumber() == meal.getShift())
+                            tableModel.addRow(new Object[] {
+                                meal.getId(),
+                                meal.getDate(), 
+                                meal.getMealNumber(),
+                                meal.getDescription(),
+                                meal.getAllergens()
+                            });
+                    }
+                    
+                    JOptionPane.showConfirmDialog(container, "Data has been imported.", "Import Successful", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE);
+                } 
+                catch (SQLException ex) 
+                {
+                    Logger.getLogger(ImportMeals.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
                 
             } 
             catch (ParseException ex) 
             {
                 Logger.getLogger(ImportMeals.class.getName()).log(Level.SEVERE, null, ex);
             }
+            catch (NullPointerException ex)
+            {
+                JOptionPane.showConfirmDialog(
+                            container,
+                            "Re-check the file structure and import again:\n"
+                                    + "Row cells MUST NOT be empty, with exception of allergens.", 
+                            "Wrong File Structure", 
+                            JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE);
+            }
 
     }
 
-  private boolean readRow(int rowNumber)
+  private boolean readRow(int rowNumber) throws ParseException
   {
       Row row = sheet.getRow(rowNumber);
-      if(row==null)
+      if(row == null)
           return false;
       
-      date = row.getCell(0, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL);
-      shift = row.getCell(1, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL);
-      mealNumber = row.getCell(2, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL);
-      mealDescription = row.getCell(3, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL);
-      allergens = row.getCell(4, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL);
-      
-      if(date == null || shift == null || mealNumber == null || mealDescription == null)
-      {
-           JOptionPane.showConfirmDialog(
-                        container,"Re-check the file structure and import again:\nRow cells MUST NOT be empty, with exception of allergens.", "Wrong File Structure", 
-                        JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE);
-          return false;
-      }
-      
+      date = formatDate(row.getCell(0, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL).getDateCellValue().toString());
+      shift = (int)row.getCell(1, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL).getNumericCellValue();
+      mealNumber = (int)row.getCell(2, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL).getNumericCellValue();
+      mealDescription = row.getCell(3, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL).getStringCellValue();
+      // allergens can be blank (null)
+      allergens = row.getCell(4, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK).getStringCellValue();
       return true;
   }
   
@@ -136,7 +150,6 @@ class ImportMeals extends ActionSettings {
         
         return String.format("%s-%02d-%02d",calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH)+1, calendar.get(Calendar.DAY_OF_MONTH));
   }
-
 }
 
 
