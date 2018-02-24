a07127f5d2310b875f036a6f8cc1b226e4ce16af
markocir <marc88cap@gmail.com>
2018-02-24 16:26:18 +0100

    Added a TextHighlighter class
    CustomTableCellRenderer can be now be used to also color the filtered 
    text match.

--- src/CustomTableCellRenderer.java
+++ src/CustomTableCellRenderer.java
@@ -1,12 +1,35 @@
+
 import java.awt.Component;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
+/**
+ *
+ * @author Marko Čirič <https://github.com/markocir>
+ */
 public class CustomTableCellRenderer extends DefaultTableCellRenderer{
+    String filterText = null;
+    
+    public CustomTableCellRenderer()
+    {
+        
+    }
+    
+    public CustomTableCellRenderer(String filterText)
+    {
+        this.filterText = filterText;
+    }
+    
+    
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-        Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column); 
+        
         setBorder(noFocusBorder);
         this.setHorizontalAlignment(DefaultTableCellRenderer.LEFT);
+        
+        TextHighlightner string = new TextHighlightner();
+        value = string.setTextToHighlight(value.toString(), filterText);
+        
+        super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column); 
         return this;
     }
 }
